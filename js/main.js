09d52d0a95d34b8a47acf147960d65403bc78a05
Lisandro Cresta <lisandrocresta99@gmail.com>
2023-05-14 13:43:55 -0300

    v-0.0.2
    -corrección de errores en main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function promedio(resultado,nota1,nota2,nota3){
+function promedio(nota1,nota2,nota3){
     if (nota3 != 0){
         resultado = (nota1 + nota2 + nota3) / 3;
     } else if(nota2 != 0) {
@@ -6,6 +6,7 @@ function promedio(resultado,nota1,nota2,nota3){
     } else {
         resultado = nota1; 
     }
+    return resultado;
 }
 
 function cadaMateria (nombreMateria, salidaMateria){
@@ -13,38 +14,36 @@ function cadaMateria (nombreMateria, salidaMateria){
     let nota2 = 0;
     let nota3 = 0;
     let resultado;
-    let cantExamenes = parseInt(prompt("¿ Rindió uno, dos o tres examenes ?"));
+    let cantExamenes = parseFloat(prompt("¿ Rindió uno, dos o tres examenes ?"));
     switch (cantExamenes) {
         case 1:
-            nota1 = parseInt(prompt("Ingrese la nota del primer examen:"));
+            nota1 = parseFloat(prompt("Ingrese la nota del primer examen:"));
             break;
         case 2:
-            nota1 = parseInt(prompt("Ingrese la nota del primer examen:"));
-            nota2 = parseInt(prompt("Ingrese la nota del segundo examen:"));
+            nota1 = parseFloat(prompt("Ingrese la nota del primer examen:"));
+            nota2 = parseFloat(prompt("Ingrese la nota del segundo examen:"));
             break;
         case 3:
-            nota1 = parseInt(prompt("Ingrese la nota del primer examen:"));
-            nota2 = parseInt(prompt("Ingrese la nota del segundo examen:"));
-            nota3 = parseInt(prompt("Ingrese la nota del tercer examen:"));
+            nota1 = parseFloat(prompt("Ingrese la nota del primer examen:"));
+            nota2 = parseFloat(prompt("Ingrese la nota del segundo examen:"));
+            nota3 = parseFloat(prompt("Ingrese la nota del tercer examen:"));
             break;
         default:
             alert("numero incorrecto de examenes ingresado");
             break;
     }
 
-    promedio(resultado,nota1,nota2,nota3);
-    /*
+    resultado = promedio(nota1,nota2,nota3);
+
     let aprobacion;
-    if (promedio > 7) {
+    if (resultado > 4) {
         aprobacion = "aprobado";
     } else {
         aprobacion = "desaprobado";
     }
-    + ", por lo tanto está " + aprobacion  
-    */
 
-    salidaMateria = salidaMateria + "En la materia " + nombreMateria + "su promedio es " + resultado + "." + "\n";
-    console.log(salidaMateria)
+    salidaMateria = salidaMateria + "En la materia " + nombreMateria + " su promedio es " + resultado.toFixed(2) + ", por lo tanto está " + aprobacion +"." + "\n";
+    return salidaMateria;
 }
 
 
@@ -54,6 +53,6 @@ let nombreMateria;
 let resultadosFinales = "";
 for (i=1;i<=cantMaterias;i++){
     nombreMateria = (prompt("Ingrese el nombre de la materia:"));
-    cadaMateria(nombreMateria,resultadosFinales);
+    resultadosFinales = cadaMateria(nombreMateria,resultadosFinales);
 }
 alert(resultadosFinales);
